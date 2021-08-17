da12b41818694622fa61ce9ca4b5f303756e124c
hicham-elkamouni <hichamelkamouni.dev@gmail.com>
2021-08-17 18:34:32 +0100

    adding vue js

---
+++ front_endd/src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <!------------ beginning of the header --------->
+  
+    <nav>
+        <input id="nav-toggle" type="checkbox">
+        <div class="logo">
+            <span>R<span style="color:#e0ac1c">CAR</span></span>
+            
+        </div>
+        <ul class="links">
+            <router-link to="/" style="color: #e0ac1c;">Home</router-link>
+            <router-link to="/contact">Contact</router-link>
+            <!-- <li><a href="index.html" >HOME</a></li> -->
+            <li><a href="gallery.html">GALLERY</a></li>
+            <li><a href="reservation.html">RESERVATION</a></li>
+            <router-link to="/contact">Contact</router-link>
+            <li><a href="about.html">ABOUT</a></li>
+            <li><a href="about.html">CONNEXION</a></li>
+
+        </ul>
+        <label for="nav-toggle" class="icon-burger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </label>
+    </nav>
+    <!------------ end of the header ------------->
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+
+@import "../assets/style/includes/navbar";
+@import "../assets/style/includes/base";
+
+
+</style>
---
+++ front_endd/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <Navbar/>
+    <Footer/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    Navbar,
+    Footer
+  }
+}
+</script>
